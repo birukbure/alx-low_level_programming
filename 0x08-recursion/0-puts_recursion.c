133423dfef101e3382aaf11b6fe76b4295215623
birukbure <birukbeletu@gmail.com>
2023-09-01 00:03:23 +0300

    dfaf

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,6 @@ void _puts_recursion(char *s)
 {
     if (*s == '\0')
     {
-        _putchar('\n'); // Print a new line when the end of the string is reached.
+        _putchar('\n'); /* Print a new line when the end of the string is reached. */
         return;
     }
-
-    _putchar(*s); // Print the current character.
-    _puts_recursion(s + 1); // Call the function recursively for the rest of the string.
-}
